8557ee3fe35c1f3ae6e04811f14939479fcc95da
Erick <ericknathancoro@hotmail.com>
2024-11-24 22:12:11 -0300

    .

--- manager.py
+++ manager.py
@@ -7,25 +7,25 @@ import threading
 #Travamento do acesso para evitar erros decorrentes de tentativas de acesso simultâneo entre as threads:
 mutex = threading.Lock()
 
-#Thread de movimentação do robô:
-def th1(self):
-    while(self.loop):
-        if(self.move):
-            if(True):
-            #if(abs(np.sqrt((self.target[0]**2)+(self.target[1]**2))-(self.sd-self.sd0))>5):
-                #print(self.sd)
-                #Lógica de controle:
-                vd = 80
-                ve = 0
-                #Envia os setpoints de velocidade para os motores:
-                rc,_ = self.client.publish('pcpy-refd', str(vd))
-                #print(mqtt.error_string(rc))
-                self.client.publish('pcpy-refe', str(ve))
-            else:
-                self.client.publish('pcpy-refd', str(0))
-                self.client.publish('pcpy-refe', str(0))
-                self.move = False
-        time.sleep(1)
+#Thread de visão computacional:
+# def th1(self):
+#     while(self.loop):
+#         if(self.move):
+#             if(True):
+#             #if(abs(np.sqrt((self.target[0]**2)+(self.target[1]**2))-(self.sd-self.sd0))>5):
+#                 #print(self.sd)
+#                 #Lógica de controle:
+#                 vd = 80
+#                 ve = 0
+#                 #Envia os setpoints de velocidade para os motores:
+#                 rc,_ = self.client.publish('pcpy-refd', str(vd))
+#                 #print(mqtt.error_string(rc))
+#                 self.client.publish('pcpy-refe', str(ve))
+#             else:
+#                 self.client.publish('pcpy-refd', str(0))
+#                 self.client.publish('pcpy-refe', str(0))
+#                 self.move = False
+#         time.sleep(1)
 
 #Classe para enviar comandos ao robô:
 class ROBO:
@@ -37,124 +37,88 @@ class ROBO:
         self.client.on_message = self.on_message
         self.client.connect(host, port)
         self.client.loop_start()
+        time.sleep(1)
         #Definindo variáveis globais do robô:
         self.loop = True
+        self.moving = False
         self.kicking = False
-        self.free_front = True
-        self.dd = 300
-        self.dc = 300
-        self.de = 300
-        self.refd = 0
-        self.refe = 0
-        self.sd = 0
-        self.se = 0
-        self.vd = 0
-        self.ve = 0
-        self.ed = 0
-        self.ee = 0
-        self.ud = 0
-        self.ue = 0
-        self.sd0 = 0
-        self.se0 = 0
-        self.move = False
-        self.target = [0,0]
-        #Declarando as threads:
-        task1 = threading.Thread(target=th1, args=(self,))
-        #Iniciando as threads:
-        task1.start()
-        #Habilita o controle remoto:
-        self.client.publish('pcpy-rc', str(1))
+        
+        # #Declarando as threads:
+        # task1 = threading.Thread(target=th1, args=(self,))
+        # #Iniciando as threads:
+        # task1.start()
     
     #Função de callback executada quando a conexão for estabelecida:
     def on_connect(self, client, userdata, flags, reason_code, properties):
         #Mensagem de status da conexão:
         print(f'Connected with result code {reason_code}')
         #Inscrevendo nos tópicos:
-        self.client.subscribe('esp32-kstate')
-        self.client.subscribe('esp32-ca')
-        self.client.subscribe('esp32-dd')
-        self.client.subscribe('esp32-dc')
-        self.client.subscribe('esp32-de')
-        self.client.subscribe('esp32-refd')
-        self.client.subscribe('esp32-refe')
-        self.client.subscribe('esp32-sd')
-        self.client.subscribe('esp32-se')
-        self.client.subscribe('esp32-vd')
-        self.client.subscribe('esp32-ve')
-        self.client.subscribe('esp32-ed')
-        self.client.subscribe('esp32-ee')
-        self.client.subscribe('esp32-ud')
-        self.client.subscribe('esp32-ue')
+        self.client.subscribe('ESP32-COM')
 
     #Função de callback executada ao receber uma mensagem:
     def on_message(self, client, userdata, msg):
-        #print(msg.topic+" "+msg.payload.decode('utf-8'))
+        print(msg.topic+" "+msg.payload.decode('utf-8'))
         #Atribuição do valor recebido à variável correspondente:
         with mutex:
-            if(msg.topic == 'esp32-kstate'):
-                self.kicking = bool(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-ca'):
-                self.free_front = bool(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-dd'):
-                self.dd = int(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-dc'):
-                self.dc = int(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-de'):
-                self.de = int(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-refd'):
-                self.refd = int(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-refe'):
-                self.refe = int(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-sd'):
-                self.sd = int(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-se'):
-                self.se = int(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-vd'):
-                self.vd = float(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-ve'):
-                self.ve = float(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-ed'):
-                self.ed = float(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-ee'):
-                self.ee = float(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-ud'):
-                self.ud = float(msg.payload.decode('utf-8'))
-            if(msg.topic == 'esp32-ue'):
-                self.ue = float(msg.payload.decode('utf-8'))
+            if(msg.topic == 'ESP32-COM'):
+                if(msg.payload.decode('utf-8') == 'K1'):
+                    self.moving = False
+                if(msg.payload.decode('utf-8') == 'L1'):
+                    self.moving = False
+                if(msg.payload.decode('utf-8') == 'R1'):
+                    self.moving = False
+                if(msg.payload.decode('utf-8') == 'G1'):
+                    self.moving = False
+                if(msg.payload.decode('utf-8') == 'A1'):
+                    self.moving = False
     
     #Função para chutar:
     def kick(self, power):
-        self.client.publish('pcpy-kick', str(power))
-        time.sleep(0.5)
+        self.client.publish('PCPY-COM', f'K{power}')
+        self.kicking = True
     
-    #Função para movimentação:
-    def gotoPos(self, x,y):
+    #Funções para movimentação:
+    def moveLinear(self, ds, power):
         with mutex:
-            self.target = [x,y]
-            self.sd0 = self.sd
-            self.se0 = self.se
-            self.move = True
+            self.client.publish('PCPY-COM', f'G{int(ds)},{int(power)}')
+            self.moving = True
+    
+    def rotate(self, dth, power):
+        with mutex:
+            self.client.publish('PCPY-COM', f'G{int(dth)},{int(power)}')
+            self.moving = True
+
+    def gotoPos(self, x, y, power):
+        with mutex:
+            self.client.publish('PCPY-COM', f'G{int(x)},{int(y)},{int(power)}')
+            self.moving = True
+
+    def align(self, th, power):
+        with mutex:
+            self.client.publish('PCPY-COM', f'G{int(th)},{int(power)}')
+            self.moving = True
     
     #Função para aguardar a movimentação:
     def waitMove(self):
         while(True):
             time.sleep(10e-3)
-            if(not self.move):
+            if(not self.moving):
+                break
+    
+    #Função para aguardar o chute:
+    def waitKick(self):
+        while(True):
+            time.sleep(10e-3)
+            if(not self.kicking):
                 break
 
     #Função para encerrar o programa (fechar as threads, etc...):
     def endProgram(self):
         self.loop = False
-        #Desabilita o controle remoto:
-        self.client.publish('pcpy-rc', str(0))
         
 
 rd = ROBO('test.mosquitto.org', 1883)
 
-rd.gotoPos(200, 200)
+#rd.moveLinear(500, 100)
 
-rd.waitMove()
-
-#rd.kick(100)
-
-rd.endProgram()
+rd.kick(100)
